d104ab05bcdd2d1f32bb0afec374f48e3a572a69
Dmytro Krasovsky <dmykras@gmail.com>
2022-12-08 14:52:41 +0100

    test rm

--- day8/main_test.go
+++ day8/main_test.go
@@ -4,23 +4,6 @@ import (
 	"testing"
 )
 
-func TestTraversal(t *testing.T) {
-	input := loadInput("./input_test.txt")
-
-	if input.TraverseNorth(3, 2) != 2 {
-		t.Fatalf("TN: want 2, got %d", input.TraverseNorth(3, 2))
-	}
-	if input.TraverseWest(3, 2) != 2 {
-		t.Fatalf("TW: want 2, got %d", input.TraverseWest(3, 2))
-	}
-	if input.TraverseSouth(3, 2) != 1 {
-		t.Fatalf("TS: want 1, got %d", input.TraverseSouth(3, 2))
-	}
-	if input.TraverseEast(3, 2) != 2 {
-		t.Fatalf("TE: want 2, got %d", input.TraverseEast(3, 2))
-	}
-}
-
 func Test(t *testing.T) {
 	input := loadInput("./input_test.txt")
 
